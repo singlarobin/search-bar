4bb51d757decb2c7aa56b6e50905016762595039
singlarobin <singlarobin99@gmail.com>
2021-11-22 11:48:05 +0530

    3 files modified

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Main from './component/main';
+import React from 'react';
 
 const App = () => {
-	return (
-		<div className="app">
-			<Main />
-		</div>
-	);
+  return (
+    <div className="app">
+      <Main />
+    </div>
+  );
 };
 
 export default App;
