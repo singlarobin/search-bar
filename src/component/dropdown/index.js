f49d46ee61a7739246f4e9227aaabc81fe7775f3
singlarobin <singlarobin99@gmail.com>
2021-11-22 11:58:12 +0530

    2 files modified

--- src/component/dropdown/index.js
+++ src/component/dropdown/index.js
@@ -2,63 +2,108 @@ import classes from './styles.module.css';
 import Product from '../product';
 import { isEmptyString, isEmptyObject } from '../../utils';
 import { Fragment, useRef, useCallback, useEffect } from 'react';
+import React from 'react';
 
-const DropDown = props => {
-    const { suggestions, collections, results, product, handleProductChange, handleOpen, errorMessage } = props;
-    const node = useRef(null);
+const DropDown = (props) => {
+  const {
+    suggestions,
+    collections,
+    results,
+    product,
+    handleProductChange,
+    handleOpen,
+    errorMessage,
+  } = props;
+  const node = useRef(null);
 
-    const handleMouseOver = e => handleProductChange(e.target.innerText);
-    const handleButtonClick = () => alert('Button functionality not Implemented!!');
+  const handleMouseOver = (e) => handleProductChange(e.target.innerText);
+  const handleButtonClick = () =>
+    alert('Button functionality not Implemented!!');
 
-    const handleClick = useCallback(e => {
-        if (node?.current?.contains(e.target)) {
-            // inside click
-            return;
-        }
-        handleOpen();
-    }, [handleOpen]);
+  const handleClick = useCallback(
+    (e) => {
+      if (node?.current?.contains(e.target)) {
+        // inside click
+        return;
+      }
+      handleOpen();
+    },
+    [handleOpen]
+  );
 
-    useEffect(() => {
-        window.addEventListener('mousedown', handleClick);
+  useEffect(() => {
+    window.addEventListener('mousedown', handleClick);
 
-        return () => document.removeEventListener('mousedown', handleClick);
-    }, [handleClick]);
+    return () => document.removeEventListener('mousedown', handleClick);
+  }, [handleClick]);
 
-    return <div ref={node} className={classes.wrapper}>
-        {isEmptyString(errorMessage) ? <div  className={classes.container}>
-            <div className={classes.leftContainer}>
-                {!isEmptyObject(suggestions) && suggestions.length !== 0 ? <Fragment>
-                    <div className={classes.listHeading}>TOP SEARCHES</div>
-                    {suggestions.map((item, index) =>
-                        <div className={classes.listItem} key={index} onMouseOver={handleMouseOver}>
-                            {item.suggestion}
-                        </div>
-                    )}
-                </Fragment> : null}
-                {!isEmptyObject(collections) && collections.length !== 0 ? <Fragment>
-                    <div className={classes.listHeading}>TOP COLLECTIONS</div>
-                    {collections.map((item, index) =>
-                        <div className={classes.listItem} key={index} onMouseOver={handleMouseOver}>
-                            {item.name}
-                        </div>
-                    )}
-                </Fragment> : null}
-            </div>
-            <div className={classes.rightContainer}>
-                {!isEmptyObject(results) && results.length !== 0 ? <Fragment>
-                    <div className={classes.productHeading}>POPULAR PRODUCTS IN ' {product} ' </div>
-                    <div className={classes.productList}>
-                        {results.map((item, index) =>
-                            <Product key={index} name={!isEmptyString(item.collectionname) ? item.collectionname : item.productname}
-                                mrpPrice={item.mrpprice} withoutDiscountPrice={item.pricewithoutdiscount} imgUrl={item.productimage} />
-                        )}
-
-                    </div>
-                </Fragment> : null}
-                <button className={classes.button} onClick={handleButtonClick}>View All Search Results</button>
-            </div>
-        </div> : <div className={classes.errorContainer}>{errorMessage}</div>}
+  return (
+    <div ref={node} className={classes.wrapper}>
+      {isEmptyString(errorMessage) ? (
+        <div className={classes.container}>
+          <div className={classes.leftContainer}>
+            {!isEmptyObject(suggestions) && suggestions.length !== 0 ? (
+              <Fragment>
+                <div className={classes.listHeading}>TOP SEARCHES</div>
+                {suggestions.map((item, index) => (
+                  <div
+                    className={classes.listItem}
+                    key={index}
+                    onMouseOver={handleMouseOver}
+                  >
+                    {item.suggestion}
+                  </div>
+                ))}
+              </Fragment>
+            ) : null}
+            {!isEmptyObject(collections) && collections.length !== 0 ? (
+              <Fragment>
+                <div className={classes.listHeading}>TOP COLLECTIONS</div>
+                {collections.map((item, index) => (
+                  <div
+                    className={classes.listItem}
+                    key={index}
+                    onMouseOver={handleMouseOver}
+                  >
+                    {item.name}
+                  </div>
+                ))}
+              </Fragment>
+            ) : null}
+          </div>
+          <div className={classes.rightContainer}>
+            {!isEmptyObject(results) && results.length !== 0 ? (
+              <Fragment>
+                <div className={classes.productHeading}>
+                  POPULAR PRODUCTS IN ' {product} '{' '}
+                </div>
+                <div className={classes.productList}>
+                  {results.map((item, index) => (
+                    <Product
+                      key={index}
+                      name={
+                        !isEmptyString(item.collectionname)
+                          ? item.collectionname
+                          : item.productname
+                      }
+                      mrpPrice={item.mrpprice}
+                      withoutDiscountPrice={item.pricewithoutdiscount}
+                      imgUrl={item.productimage}
+                    />
+                  ))}
+                </div>
+              </Fragment>
+            ) : null}
+            <button className={classes.button} onClick={handleButtonClick}>
+              View All Search Results
+            </button>
+          </div>
+        </div>
+      ) : (
+        <div className={classes.errorContainer}>{errorMessage}</div>
+      )}
     </div>
+  );
 };
 
 export default DropDown;
